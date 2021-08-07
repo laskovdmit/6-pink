68ff4c2082a3a8f376203cec70f45166c830a3ff
Dima <laskovdmit@gmail.com>
2021-08-08 01:54:50 +0500

    Начальная стилизация с помощью SASS мобильной версии сайта. Подключил и настроил сборщик Gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
-var sourcemaps = require('gulp-sourcemaps');
-var watch = require('gulp-watch');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const watch = require('gulp-watch');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
-gulp.task('sass-compile', function(){
-    return gulp.src('./sass/**/*.scss')
+gulp.task('sass', function(){
+    return gulp.src('./sass/style.scss')
     .pipe(sourcemaps.init())
+    .pipe(postcss([ autoprefixer() ]))
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('./sass/**/*.scss', gulp.series('sass-compile'));
+    gulp.watch('./sass/**/*.scss', gulp.series('sass'));
 });
